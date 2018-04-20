261c1f4e26f40f9a8ba0ccedaeeba1bddc9a65b3
Vai na Web <vainawebcpx@gmail.com>
2018-04-20 15:51:20 -0300

    quase pronto2

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,26 +1,49 @@
 <template>
-	<div>
-		<section>
-			<img src="../assets/pc1.jpg">
-			<div>
-				<h1>OUR PRODUCT</h1>
-				<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-				<p>Quisque volutpat mattis eros.</p>
-				<p>Nullam malesuada erat ut turpis.</p>
-				<button>Read more</button>
-			</div>
-		</section>
-		<section>
-			<div>
-				<h1>PRODUCT DESCRIPTIONS</h1>
-				<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
-				<p>Quisque volutpat mattis eros.</p>
-				<p> Nullam malesuada erat ut turpis.</p>
-				<button>Read more</button>
-			</div>
-			<img src="../assets/pc2.jpg">
-		</section>
+	<div id="Appproduct">
+		<div class="container">
+			<section class="container__partup">
+				<img class="container__partup--rightcomputer" src="../assets/pc1.jpg">
+				<div class="container__partup">
+					<h1 class="container__partup--title">OUR PRODUCT</h1>
+					<p class="container__partup--subtitlepart1">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+					<p class="container__partup--subtitlepart2">Quisque volutpat mattis eros.</p>
+					<p class="container__partup--subtitlepart3">Nullam malesuada erat ut turpis.</p>
+					<button class="container__partup--holdtight">Read more</button>
+				</div>
+			</section>
+			<section class="container__partlow">
+				<div class="container__partlow">
+					<h1 class="container__partlow--title">PRODUCT DESCRIPTIONS</h1>
+					<p class="container__partlow--subtitlepart">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+					<p class="container__partlow--subtitlepart">Quisque volutpat mattis eros.</p>
+					<p class="container__partlow--subtitlepart"> Nullam malesuada erat ut turpis.</p>
+					<button class="container__partlow--holdtight">Read more</button>
+				</div>
+				<img class="container__partlow--leftcomputer" src="../assets/pc2.jpg">
+			</section>
+		</div>
 	</div>
 </template>
 <script></script>
-<style scoped></style>
+<style scoped>
+	#Appproduct{
+
+	}
+	.container{
+		min-height: 100vh;
+	}
+	.container__partup{
+		display: flex;
+		flex-direction: row;
+	}
+	.container__partup{
+		display: flex;
+		flex-direction: column;
+	}
+	.container__partup--title{
+
+	}
+	.container__partlow--subtitlepart{
+		font-size: 20px;
+	}
+</style>
--- src/components/SoftTech.vue
+++ src/components/SoftTech.vue
@@ -3,17 +3,29 @@
 		<section class="container">
 			<img class="container__image" src="../assets/logo1.png">
 			<nav class="container__list">
-				<ol class="container__list--content">HOME</ol>
-				<ol class="container__list--content">PRODUCT</ol>
+				<ol class="container__list--content" v-on:click="SoftTechHome">HOME</ol>
+				<ol class="container__list--content" v-on:click="Product">PRODUCT</ol>
 				<ol class="container__list--content">SERVICE</ol>
 				<ol class="container__list--content">BUSINESS</ol>
 				<ol class="container__list--content">CONTACT</ol>
 			</nav>
 			<button class="container__list--action">SIGN IN</button>
 		</section>
-	</div>
+	</div> 
 </template>
-<script></script>
+<script>
+import SoftTechHome from '@/components/SoftTechHome'
+import Product from '@/components/Product'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return{
+
+    }
+  }
+}
+</script>
 <style scoped>
 #AppSite{
 	background-color: red;
@@ -34,12 +46,15 @@
 	flex-direction: row;
 	color: #82CA9C;
 	margin-top: 10px;
-	margin-left: 30em;
+	margin-left: 9em;
 }
 .container__list--content{
 	/*background-color: tomato;*/
 	margin: 10px;
 	color: black;
+}
+.container__list--content:hover{
+	color: #82CA9C;
 }
 .container__list--action{
 	/*background-color: yellow;*/
@@ -50,5 +65,9 @@
 	color: #82CA9C;
 	background-color: #fff;
 	border-radius: 50px;
+}
+.container__list--action:hover{
+	background: #82CA9C;
+	color: #fff;
 }
 </style>
